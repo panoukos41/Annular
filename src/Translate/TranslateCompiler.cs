f5ad91a0956c6a223cd9b6a5865e20133d7f9c16
Panos <panos_ath@outlook.com>
2024-05-20 00:30:26 +0300

    add: TranslateCompiler

---
+++ src/Translate/TranslateCompiler.cs
@@ -0,0 +1,8 @@
+﻿namespace Annular.Translate;
+
+public abstract class TranslateCompiler
+{
+    public abstract string Compile(string value, string lang);
+
+    public abstract Translations CompileTranslations(Translations translations, string lang);
+}
