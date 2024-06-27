e380304f2d5908ca9a9c59fa6705605840aa9f2d
Panos Athanasiou <panos_ath@outlook.com>
2024-06-27 18:45:09 +0300

    Add Feature Modules (#2)
    * update packages
    
    * add: Modules src and test projects
    
    * add: version.json
    
    * update(workflows): _ci.yaml to run test project
    
    * add(workflows): modules build & publish
    
    * update(workflows): translate build & publish

---
+++ src/Modules/IModule.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Annular.Modules;
+
+/// <summary>
+/// Defines a module. Modules contain all the services
+/// and other modules needed for the module to function correctly.
+/// eg: The WeatherModule should register it's WeatherService and WeatherStore implementations
+/// for use in the application later. Modules can also act as options for runtime behavior.
+/// </summary>
+public interface IModule
+{
+    /// <summary>
+    /// Method to add module services to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <param name="configuration">The provided IConfiguration.</param>
+    abstract static void Add(IServiceCollection services, IConfiguration configuration);
+}
---
+++ tests/Modules.Tests/Abstract/TestConfigurationProvider.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Annular.Modules.Tests.Abstract;
+
+internal sealed class TestConfigurationProvider : ConfigurationProvider, IConfigurationSource
+{
+    public void Reload() => OnReload();
+
+    public IConfigurationProvider Build(IConfigurationBuilder builder) => this;
+}
