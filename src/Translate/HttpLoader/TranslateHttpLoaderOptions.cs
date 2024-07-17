3042c52a477772e7852cd16ddc7e87560c0bef62
Panos <panos_ath@outlook.com>
2024-07-17 21:49:51 +0300

    fix: TranslateHttpLoaderOptions default Prefix

--- src/Translate/HttpLoader/TranslateHttpLoaderOptions.cs
+++ src/Translate/HttpLoader/TranslateHttpLoaderOptions.cs
@@ -2,7 +2,7 @@
 
 public class TranslateHttpLoaderOptions
 {
-    public string Prefix { get; set; } = "/i18n/";
+    public string Prefix { get; set; } = "i18n/";
 
     public string Suffix { get; set; } = ".json";
 }
